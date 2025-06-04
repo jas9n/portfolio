1d8773b4be4fa1182df3fbad1199c4d857ceab74
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2025-06-04 12:01:47 -0400

    Update route.ts

--- src/app/api/spotify/auth/route.ts
+++ src/app/api/spotify/auth/route.ts
@@ -1,13 +1,15 @@
 import { NextResponse } from 'next/server';
 
-const client_id = '76377f27b4b54be08fbde22f450e13e0';
-const redirect_uri = 'https://jas9n.vercel.app/api/spotify/callback';
-const scope = 'user-read-currently-playing user-read-playback-state';
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const redirect_uri = process.env.VERCEL_URL 
+  ? `https://${process.env.VERCEL_URL}/api/spotify/callback`
+  : 'http://localhost:3000/api/spotify/callback';
+const scope = 'user-read-currently-playing user-read-playback-state user-read-recently-played';
 
 export async function GET() {
   const params = new URLSearchParams({
     response_type: 'code',
-    client_id,
+    client_id: client_id!,
     scope,
     redirect_uri,
   });
