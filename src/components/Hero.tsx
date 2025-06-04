16efe953f38e934971312b49e3327544e0174108
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2025-06-04 12:53:34 -0400

    hero styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
     {
       id: 1,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center">
+        <div className="h-8 md:h-auto flex items-center">
           <span className="text-emerald-500 tracking-tight text-5xl lg:text-6xl">Jason Chen</span>
         </div>
       )
@@ -61,7 +61,7 @@ const Hero = () => {
     {
       id: 2,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-gray-900 tracking-tight">from</span>
           <span className="text-emerald-500 tracking-tight">Brooklyn, NY</span>
         </div>
@@ -70,7 +70,7 @@ const Hero = () => {
     {
       id: 3,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-gray-900 tracking-tight">a</span>
           <span className="text-emerald-500 tracking-tight">software engineer</span>
         </div>
@@ -79,7 +79,7 @@ const Hero = () => {
     {
       id: 4,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-gray-900 tracking-tight">writing</span>
           <TypewriterText text="<code/>" isVisible={currentSlide === 3} />
         </div>
@@ -88,7 +88,7 @@ const Hero = () => {
     {
       id: 5,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-gray-900 tracking-tight">into</span>
           <span className="text-emerald-500 tracking-tight">fashion</span>
           <span className="text-gray-900 tracking-tight">&</span>
@@ -99,7 +99,7 @@ const Hero = () => {
     {
       id: 6,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-gray-900 tracking-tight">turning</span>
           <span className="text-emerald-500 tracking-tight">ideas</span>
           <span className="text-gray-900 tracking-tight">into</span>
@@ -110,7 +110,7 @@ const Hero = () => {
     {
       id: 7,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-emerald-500 tracking-tight">learning</span>
           <span className="text-gray-900 tracking-tight">something</span>
           <span className="text-emerald-500 tracking-tight">new</span>
@@ -121,7 +121,7 @@ const Hero = () => {
     {
       id: 8,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center">
+        <div className="h-8 md:h-auto flex items-center">
           <div className="flex items-center space-x-2 md:space-x-3 lg:space-x-4">
             <span className="text-gray-900 tracking-tight whitespace-nowrap">listening to</span>
             <SpotifyNowPlaying isVisible={currentSlide === 7} />
@@ -132,7 +132,7 @@ const Hero = () => {
     {
       id: 9,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-gray-900 tracking-tight">a</span>
           <span className="text-emerald-500 tracking-tight">movie</span>
           <span className="text-emerald-500 tracking-tight">enthusiast</span>
@@ -142,7 +142,7 @@ const Hero = () => {
     {
       id: 10,
       content: (
-        <div className="h-8 sm:h-20 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="h-8 md:h-auto flex items-center space-x-2 md:space-x-3 lg:space-x-4">
           <span className="text-emerald-500 tracking-tight">debugging</span>
           <span className="text-gray-900 tracking-tight">my</span>
           <span className="text-emerald-500 tracking-tight">life</span>
@@ -187,7 +187,7 @@ const Hero = () => {
           <div className="text-6xl md:text-5xl lg:text-6xl font-bold mb-4 md:mb-0 whitespace-nowrap tracking-tight">
             Hello! I am
           </div>
-          <div className="text-3xl font-bold md:text-5xl lg:text-6xl">
+          <div className="text-3xl ml-0.5 md:ml-0 font-bold md:text-5xl lg:text-6xl">
             <AnimatePresence mode="wait">
               <motion.div
                 key={currentSlide}
@@ -205,7 +205,7 @@ const Hero = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.5 }}
-          className="flex justify-start items-center mt-8"
+          className="flex justify-start items-center mt-8 ml-1"
         >
           <Link
             href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=jasonc3805@gmail.com"
